444ad1e7c1902e1a2ce4b83e4338409b38112cb5
Benjamin Pannell <benjamin@pannell.dev>
2020-05-18 21:12:02 +0100

    feat: Add an API to get the counts for each service

--- honeypot/api.go
+++ honeypot/api.go
@@ -9,17 +9,32 @@ import (
 
 func (h *Host) RunAPI(addr string) error {
 	r := mux.NewRouter()
-	r.Path("/api/v1/stats").Methods("GET").Handler(girder.NewHandler(h.getState))
+	r.Path("/api/v1/stats").Methods("GET").Handler(girder.NewHandler(h.getStats))
+	r.Path("/api/v1/counts").Methods("GET").Handler(girder.NewHandler(h.getCounts))
 	r.Path("/api/v1/health").Methods("GET").Handler(girder.NewHandler(h.getHealth))
 
 	return http.ListenAndServe(":8080", r)
 }
 
-func (h *Host) getState(c *girder.Context) (interface{}, error) {
+func (h *Host) getStats(c *girder.Context) (interface{}, error) {
 	h.State.m.RLock()
 	defer h.State.m.RUnlock()
 
 	return h.State, nil
+}
+
+func (h *Host) getCounts(c *girder.Context) (interface{}, error) {
+	h.State.m.RLock()
+	defer h.State.m.RUnlock()
+
+	out := map[string]uint64{}
+
+	for _, svc := range h.State.Services {
+		out[svc.Name] = svc.Attempts
+		out["_total"] += svc.Attempts
+	}
+
+	return out, nil
 }
 
 func (h *Host) getHealth(c *girder.Context) (interface{}, error) {
